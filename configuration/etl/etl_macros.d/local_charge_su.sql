ac0a44710096c661939caf4dcc6bbbbbca69ff07
Alex Tovar <alex@Mac-164.lan>
2025-03-14 14:57:44 -0400

    created a default set up for local_charge_su

--- configuration/etl/etl_macros.d/local_charge_su.sql
+++
@@ -1,4 +0,0 @@
-case
-    when 1=1 then 1
-    else 0
-end
---
+++ configuration/etl/etl_sql.d/jobs/job-record-local_charge-post-ingest.sql
@@ -0,0 +1,17 @@
+UPDATE
+    ${DESTINATION_SCHEMA}.job_records records
+    JOIN
+    ${DESTINATION_SCHEMA}.resourcefact rf ON records.resourcetype_id = rf.resourcetype_id
+    JOIN
+    ${DESTINATION_SCHEMA}.resource_allocation_type rat ON rf.resource_allocation_type_id = rat.resource_allocation_type_id
+    JOIN
+    ${DESTINATION_SCHEMA}.job_tasks tasks on records.job_record_id = tasks.job_record_id
+    SET
+        records.local_charge_su =
+        CASE
+        WHEN rat.resource_allocation_type_abbrev = 'CPU' THEN 1 * tasks.processor_count * (tasks.wallduration / 3600)
+        WHEN rat.resource_allocation_type_abbrev = 'GPU' THEN 1 * tasks.gpu_count * (tasks.wallduration / 3600)
+        WHEN rat.resource_allocation_type_abbrev = 'CPUNode' THEN 1 * tasks.node_count * (tasks.wallduration / 3600)
+        WHEN rat.resource_allocation_type_abbrev = 'GPUNode' THEN 1 * tasks.gpu_count * (tasks.wallduration / 3600)
+END;
+
