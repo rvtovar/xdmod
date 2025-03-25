7bf9e90713c181c8e94698c98f090028169c4685
Alex Tovar <alex@Mac-1249.lan>
2025-03-25 12:04:14 -0400

    made change to floating arthemtic

--- configuration/etl/etl_sql.d/jobs/job-record-local_charge-post-ingest.sql
+++ configuration/etl/etl_sql.d/jobs/job-record-local_charge-post-ingest.sql
@@ -1,17 +1,15 @@
 UPDATE
-    ${DESTINATION_SCHEMA}.job_records records
+    ${DESTINATION_SCHEMA}.job_tasks tasks
     JOIN
-    ${DESTINATION_SCHEMA}.resourcefact rf ON records.resource_id = rf.id
+    ${DESTINATION_SCHEMA}.resourcefact rf ON tasks.resource_id = rf.id
     JOIN
     ${DESTINATION_SCHEMA}.resource_allocation_type rat ON rf.resource_allocation_type_id = rat.resource_allocation_type_id
-    JOIN
-    ${DESTINATION_SCHEMA}.job_tasks tasks on records.job_record_id = tasks.job_record_id
     SET
-        records.local_charge_su =
+        tasks.local_charge_su =
         CASE
-        WHEN rat.resource_allocation_type_abbrev = 'CPU' THEN 1 * tasks.processor_count * (tasks.wallduration / 3600)
-        WHEN rat.resource_allocation_type_abbrev = 'GPU' THEN 1 * tasks.gpu_count * (tasks.wallduration / 3600)
-        WHEN rat.resource_allocation_type_abbrev = 'CPUNode' THEN 1 * tasks.node_count * (tasks.wallduration / 3600)
-        WHEN rat.resource_allocation_type_abbrev = 'GPUNode' THEN 1 * tasks.gpu_count * (tasks.wallduration / 3600)
+        WHEN rat.resource_allocation_type_abbrev = 'CPU' THEN 1.0 * tasks.processor_count * (tasks.wallduration / 3600.0)
+        WHEN rat.resource_allocation_type_abbrev = 'GPU' THEN 1.0 * tasks.gpu_count * (tasks.wallduration / 3600.0)
+        WHEN rat.resource_allocation_type_abbrev = 'CPUNode' THEN 1.0 * tasks.node_count * (tasks.wallduration / 3600.0)
+        WHEN rat.resource_allocation_type_abbrev = 'GPUNode' THEN 1.0 * tasks.gpu_count * (tasks.wallduration / 3600.0)
 END;
 
